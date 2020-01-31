3b314c22e7f45ca0ce5f02816b31459f07473af8
juanls85 <juanls85@protonmail.com>
2020-01-31 13:39:05 -0700

    added basic functionality of calculator

---
+++ script.js
@@ -0,0 +1,5 @@
+const add = (n1, n2) => n1 + n2;
+const subtract = (n1, n2) => n1 - n2;
+const multiply = (n1, n2) => n1 * n2;
+const divide = (n1, n2) => n1 / n2;
+const operate = (op, n1, n2) => op(n1, n2);
